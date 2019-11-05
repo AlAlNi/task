6db82a9a98a5cb76a87f7c35adfb51eda8037038
Alex N <dobrijserv@gmail.com>
2019-11-05 20:28:09 +0300

    pereimenoval classs

---
+++ src/part1/lesson02/task03/FillPerson.java
@@ -0,0 +1,23 @@
+package part1.lesson02.task03;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class FillPerson {
+    static void fillPersons() {
+        for (int i = 0; i < OsnMain.nA; i++) {
+            Random n = new Random();
+            Random m = new Random();
+            Sex s = n.nextInt(10) % 2 == 0 ? Sex.MAN : Sex.WOMAN;
+            ArrayList<Person> names = s == Sex.MAN ? OsnMain.obNameMan : OsnMain.obNameWoman;
+            Person person = new Person(m.nextInt(101), s, names.get(m.nextInt(names.size())).name);
+            if (OsnMain.obPerson.size() <= i) {
+
+                OsnMain.obPerson.add(person);
+            } else {
+                OsnMain.obPerson.set(i, person);
+            }
+//        System.out.println(i);
+        }
+    }
+}
--- src/part1/lesson02/task03/OsnMain.java
+++ src/part1/lesson02/task03/OsnMain.java
@@ -23,16 +23,17 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class OsnMain {
-    private static ArrayList<Person> obPerson = new ArrayList<>();
-    private static int nA;
-    private static ArrayList<Person> obNameMan = new ArrayList<>();
-    private static ArrayList<Person> obNameWoman = new ArrayList<>();
+    public static int minCh;
+    public static int nA;
+
+    public static ArrayList<Person> obPerson = new ArrayList<>();
+    public static ArrayList<Person> obNameMan = new ArrayList<>();
+    public static ArrayList<Person> obNameWoman = new ArrayList<>();
 
     public static void main(String[] args) throws IOException, DubNameAgeException, MinChException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-
-        int minCh = 10000;
+        minCh  = 10000;
 
 
         System.out.println("Введите список мужских имён в конце списка наберите: end или конец");
@@ -70,35 +71,21 @@ public class OsnMain {
             }
         }
 
-        fillPersons();
+        FillPerson.fillPersons();
 //        System.out.println(obPerson.toString());
         Sorter sort1 = new Sorter1();
         long sortTime1 = sort1.sort(obPerson);
 //        System.out.println(obPerson.toString());
         System.out.println("Время, потраченное на сортировку массива методом пузырька: " + sortTime1 / 1000. + " сек.");
 
-        fillPersons();
+        FillPerson.fillPersons();
         Sorter sort2 = new Sorter2();
         long sortTime2 = sort2.sort(obPerson);
         //System.out.println(Arrays.toString(peoples));
         System.out.println("Время, потраченное на сортировку встроенными методами класса Arrays: " + sortTime2 / 1000. + " сек.");
     }
 
-    private static void fillPersons() {
-        for (int i = 0; i < nA; i++) {
-            Random n = new Random();
-            Random m = new Random();
-            Sex s = n.nextInt(10) % 2 == 0 ? Sex.MAN : Sex.WOMAN;
-            ArrayList<Person> names = s == Sex.MAN ? obNameMan : obNameWoman;
-            Person person = new Person(m.nextInt(101), s, names.get(m.nextInt(names.size())).name);
-            if (obPerson.size() <= i) {
 
-                obPerson.add(person);
-            } else {
-                obPerson.set(i, person);
-            }
-//        System.out.println(i);
-        }
 
-    }
+
 }
